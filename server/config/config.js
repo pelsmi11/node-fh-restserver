a800ea7a554e17fdc1fb28e664b3a7e0509a3e7f
pelsmi11 <hmartinezmoreira81@gmail.com>
2020-12-09 15:00:16 -0600

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,37 @@
 //=========================
 
 process.env.PORT = process.env.PORT || 3000;
+
+//=========================
+// ENTORNO
+//=========================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+
+//=========================
+// PUERTO
+//=========================
+
+let urlDB
+
+//if (process.env.NODE_ENV === 'dev') {
+//  urlDB = 'mongodb://localhost:27017/cafe';
+
+//} else {
+urlDB = 'mongodb+srv://pelsmi11:faztpassword@cluster0.tno1u.mongodb.net/cafe?retryWrites=true&w=majority';
+//}
+
+
+/* const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://pelsmi11:<faztpassword>@cluster0.tno1u.mongodb.net/<cafe>?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("cafe");
+  // perform actions on the collection object
+  client.close();
+}); */
+
+
+process.env.URLDB = urlDB;
